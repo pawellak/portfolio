a218437cd804a4bff4a10686dcc2d72fccd3b30b
pawellak <pawellak@gmail.com>
2025-09-22 19:10:21 +0200

    add: pico project

--- packages/features/project_list/lib/presentation/project_page/widgets/project_expanded_widget.dart
+++ packages/features/project_list/lib/presentation/project_page/widgets/project_expanded_widget.dart
@@ -3,6 +3,7 @@ import 'package:components_library/resources/dimens.dart';
 import 'package:components_library/widgets/container/cl_container.dart';
 import 'package:feature_project_list/presentation/data/project_model.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart'
     show BoxFit, BuildContext, Colors, Column, EdgeInsets, Expanded, Row, SizedBox, StatelessWidget, Widget;
 
@@ -18,12 +19,13 @@ class ProjectExpandedWidget extends StatelessWidget {
     child: Column(
       spacing: Dimens.dimen36,
       children: [
-        const SizedBox(height: Dimens.dimen12),
+        const SizedBox(),
         _ProjectSectionHtml(description: project.introduction, imagePath: project.introductionUrl),
         if (project.development.isNotEmpty)
           _ProjectSectionHtml(description: project.development, imagePath: project.developmentUrl),
         if (project.conclusion.isNotEmpty)
           _ProjectSectionHtml(description: project.conclusion, imagePath: project.conclusionUrl),
+        if (kIsWeb) const SizedBox(),
       ],
     ),
   );
@@ -37,7 +39,12 @@ class _ProjectSectionHtml extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ClContainer(
-    padding: const EdgeInsets.only(left: Dimens.dimen24,right: Dimens.dimen8,bottom: Dimens.dimen24,top: Dimens.dimen24),
+    padding: const EdgeInsets.only(
+      left: Dimens.dimen24,
+      right: Dimens.dimen8,
+      bottom: Dimens.dimen24,
+      top: Dimens.dimen24,
+    ),
     child: Row(
       children: [
         Expanded(flex: _kLabelFlex, child: ClHtmlLabel(data: description)),
